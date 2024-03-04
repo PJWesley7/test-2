1e45783d6257e83f2a1144e6849f44d230c0cecd
pjwesley7 <pjwesley.chintalapudi@gmail.com>
2024-03-04 23:31:10 +0530

    changed the url

--- src/test/java/com/test/JenkinsTest2/AppTest.java
+++ src/test/java/com/test/JenkinsTest2/AppTest.java
@@ -14,7 +14,7 @@ public class AppTest {
 	@Test
 	public void testApiDataMatch() throws Exception {
 		String apiUrl1 = "https://dummyjson.com/products/1";
-		String apiUrl2 = "https://dummyjson.com/products/1";
+		String apiUrl2 = "https://dummyjson.com/products/2";
 
 		String dataFromApi1 = getDataFromApi(apiUrl1);
 		String dataFromApi2 = getDataFromApi(apiUrl2);
